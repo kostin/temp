c3af6ebf724712ba28ddf5ccaaab7e6fa8df05ac
Alexey Kostin <kostin@users.noreply.github.com>
2015-04-18 00:04:48 +0300

    Update cachewarmer.sh

--- cachewarmer.sh
+++ cachewarmer.sh
@@ -9,7 +9,7 @@ fi
 
 echo $(date +'%Y-%m-%d %H:%S')" Warmer starting..." >> /var/log/warmer.log
 
-/usr/bin/curl --silent  http://www.gssportng.s08.yazato.ru/sitemap.xml | egrep -o "http://$URL[^<\"> ]+" > /var/log/sitemap.xml
+/usr/bin/curl --silent  http://www.gssport.s08.yazato.ru/sitemap.xml | egrep -o "http://$URL[^<\"> ]+" > /var/log/sitemap.xml
 
 echo $(date +'%Y-%m-%d %H:%S')" Warmer queue length is "$(cat /var/log/sitemap.xml | wc -l) >> /var/log/warmer.log
 
@@ -19,13 +19,10 @@ do
   URL="${LINE}"
   RES=$(/usr/bin/curl --silent --max-time 5 -IL "$URL")
   STOP=$(date +%s%N | cut -b1-13)
-  CODE=$(echo "$res" | grep -e 'HTTP' | tr -d '\n\r')
-  HIT=$(echo "$res" | grep -e 'X-Nginx-Cache' | tr -d '\n\r')
+  CODE=$(echo "${RES}" | grep -e 'HTTP' | tr -d '\n\r')
+  HIT=$(echo "${RES}" | grep -e 'X-Nginx-Cache' | tr -d '\n\r')
   TIME=$((STOP-START))
-  echo $(date +'%Y-%m-%d %H:%S')" Warm ${URL} in ${TIME}ms ${CODE} ${HIT}" >> /var/log/warmer.log
+  echo $(date +'%Y-%m-%d %H:%S')" Warmed in ${TIME}ms url ${URL} with ${CODE} ${HIT}" >> /var/log/warmer.log
 done
 
 echo $(date +'%Y-%m-%d %H:%S')" Warmer finished..." >> /var/log/warmer.log
-
-# add to cron
-# 40 */3 * * * /opt/scripts/cachewarmer.sh > /dev/null 2>&1
